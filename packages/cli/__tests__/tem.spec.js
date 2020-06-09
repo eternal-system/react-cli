0f087037c2269acf25d6dec7b3f7ff16a4daa391
Corvax <vlvete1990@yandex.ru>
2020-06-09 17:24:45 +0300

    chore: adding jest

---
+++ packages/cli/__tests__/tem.spec.js
@@ -0,0 +1,4 @@
+
+test('my first test', () => {
+  expect(1 + 1).toBe(2)
+})
--- packages/cli/lib/ui.js
+++ packages/cli/lib/ui.js
@@ -1,35 +1,32 @@
-
 // const server = require('@react/cli-ui');
-const { server } = require('../../cli-ui/server');
-const { openBrowser } = require('./util/openBrowser');
-const port = 8080;
+const { server } = require('../../cli-ui/server')
+const { openBrowser } = require('./util/openBrowser')
+const port = 8080
 
 const ui = (args = {}) => {
-    const host = args.host || 'localhost'
-    console.log('ui arg', args)
+  const host = args.host || 'localhost'
+  console.log('ui arg', args)
 
-    if (!args.quiet) console.log(`🚀  Starting GUI...`)
+  if (!args.quiet) console.log('🚀  Starting GUI...')
 
-    // Config for server enveronment
-    const config = {
-        host,
-        port
+  // Config for server enveronment
+  const config = {
+    host,
+    port
+  }
+
+  // start server
+  server(config, () => {
+    // Open browser
+    const url = `http://${host}:${port}`
+    if (!args.quiet) console.log(`🌠  Ready on ${url}`)
+
+    if (args.headless) {
+      console.log(port)
+    } else {
+      openBrowser(url)
     }
-
-    // start server
-    server(config, () => {
-
-         // Open browser
-        const url = `http://${host}:${port}`
-        if (!args.quiet) console.log(`🌠  Ready on ${url}`)
-        
-        if (args.headless) {
-            console.log(port)
-          } else {
-            openBrowser(url)
-          }
-        
-    });
+  })
 }
 
-module.exports = ui;
+module.exports = ui
