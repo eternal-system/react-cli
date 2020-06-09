15a87c49b88d8d7e62878417847a8e044874e061
Corvax <vlvete1990@yandex.ru>
2020-06-09 14:26:37 +0300

    chore: clear server demo

--- packages/cli-ui/server.dev.js
+++ packages/cli-ui/server.dev.js
@@ -1,62 +1,38 @@
-const http = require('http');
+/**
+ * Cheak folder dist
+ * 
+ * 1. If /dist exists to stream file => res
+ * 2. If /dist !exists to webpack build new /dist
+ */
+
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 8080
+const app = express()
 const fs = require('fs');
-const path = require('path');
 
-const httpServer = http.createServer((req, res) => {
-    // res.writeHead(200, {
-    //     'Content-Type': 'text/html',
-    //     'Expires': '0'
-    // });
- 
-   // console.log(path.join(__dirname, 'dist', 'index.html'));
-   
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js');
 
-    let filePath = '.' + req.url;
-    if (filePath == './') {
-        filePath = './index.html';
+app.use(express.static(__dirname + '/dist'));
+
+app.get('*', function (req, res){
+
+    const filePath = path.resolve(__dirname, 'dist', 'index.html');
+  
+    if (fs.existsSync(filePath)) {
+        fs.createReadStream(filePath).pipe(res);
+    } else {
+        webpack(webpackConfig, (err, stats) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            fs.createReadStream(filePath).pipe(res);
+        }) 
     }
 
-    let extname = String(path.extname(filePath)).toLowerCase();
-    let mimeTypes = {
-        '.html': 'text/html',
-        '.js': 'text/javascript',
-        '.css': 'text/css',
-        '.json': 'application/json',
-        '.png': 'image/png',
-        '.jpg': 'image/jpg',
-        '.gif': 'image/gif',
-        '.wav': 'audio/wav',
-        '.mp4': 'video/mp4',
-        '.woff': 'application/font-woff',
-        '.ttf': 'application/font-ttf',
-        '.eot': 'application/vnd.ms-fontobject',
-        '.otf': 'application/font-otf',
-        '.svg': 'application/image/svg+xml'
-    };
-
-    let contentType = mimeTypes[extname] || 'application/octet-stream';
-    // console.log("contentType", contentType);
-   
-    res.writeHead(200, { 'Content-Type': contentType });   
-
-    // path.join(__dirname, 'dist', 'index.html')
-     fs.readFile(path.resolve(__dirname, 'dist', 'index.html'), 'utf-8', (err, text) => {
-        if (err) {
-            throw new Error('Error load file')
-        }
-
-        console.log(text)
-    
-        res.writeHead(200, { 'Content-Type': contentType });   
-        res.end(text, 'utf-8');
-    })
-  //  fs.createReadStream(path.join(__dirname, '/dist/index.html'), 'utf-8').pipe(res);
-})
-    
-httpServer.listen(
-    '8080', 
-    'localhost'
-,() => {
-    console.log('starter server on http://localhost:8080')
 });
 
+app.listen(port);
+console.log("server started on port " + port);
--- packages/cli-ui/server.js
+++ packages/cli-ui/server.js
@@ -1,10 +1,34 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+/**
+ * Cheak folder dist
+ * 
+ * 1. If /dist exists to stream file => res
+ * 2. If /dist !exists to webpack build new /dist
+ */
+const express       =   require('express');
+const path          =   require('path');
+const app           =   express();
+const fs            =   require('fs');
+
+const webpack       =   require('webpack');
+const webpackConfig =   require('./webpack.config.js');
 
 module.exports.server = (options, cb = null) => {
   
     app.use(express.static(__dirname + '/dist'))
+
+    const filePath = path.resolve(__dirname, 'dist', 'index.html');
+  
+    if (fs.existsSync(filePath)) {
+            fs.createReadStream(filePath).pipe(res);
+    } else {
+        webpack(webpackConfig, (err, stats) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            fs.createReadStream(filePath).pipe(res);
+        }) 
+    }
 
     app.get('*', function (req, res){ 
         const filePath = path.resolve(__dirname, 'dist', 'index.html');
