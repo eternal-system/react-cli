a9718643744449e2731b0d11651687cb2f5f0c89
Corvax <vlvete1990@yandex.ru>
2020-06-21 14:29:36 +0300

    chore: #14 add terminal kill port

--- packages/cli/lib/kill.js
+++ packages/cli/lib/kill.js
@@ -1,5 +1,12 @@
-function kill (port, options) {
-  console.log('kill', port, options)
+const fkill = require('fkill')
+
+async function kill (port, options) {
+  try {
+    await fkill(`:${port}`)
+    console.log(`🌠 Port :${port} successfully killed`)
+  } catch (error) {
+    console.log(e)
+  }
 }
 
 module.exports = kill
