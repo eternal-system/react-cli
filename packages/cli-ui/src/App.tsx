702d2a043e6627279fd914541bdbaf5bcafc7d56
Denis Isaev <denis.isaev@rt.ru>
2020-06-22 23:40:44 +0300

    feature | #17 | include i18n in App component
    - Подключил локализацию в компоненте App

--- packages/cli-ui/src/App.tsx
+++ packages/cli-ui/src/App.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import { Context } from './context'
+import { I18nextProvider } from 'react-i18next'
+import i18n from './i18n'
 
+import { Context } from './context'
 import { renderRoutes } from './router'
 
 export default function App () {
   const getLocalStorage = JSON.parse(localStorage.getItem('thememode'))
   const theme = getLocalStorage ? 'dark' : 'ligth'
- 
+
   const [value, setValue] = useState(getLocalStorage)
 
   useEffect(() => {
@@ -16,10 +18,12 @@ export default function App () {
   const getValue = (status) => setValue(status)
 
   return (
-    <Context.Provider value={{getValue}}>
-      <div key={theme} className={`wrapper content ${theme}`}>
-        {renderRoutes()}
-      </div>
-    </Context.Provider>
+    <I18nextProvider i18n={i18n}>
+      <Context.Provider value={{ getValue }}>
+        <div key={theme} className={`wrapper content ${theme}`}>
+          {renderRoutes()}
+        </div>
+      </Context.Provider>
+    </I18nextProvider>
   )
 }
