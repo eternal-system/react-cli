e4fec63df608cafaad1be217d336d21b10d723a7
Denis Isaev <denis.isaev@rt.ru>
2020-06-23 21:54:04 +0300

    feature | #17 | locale projectContainer.hook
    - Локализировал projectContainer hook

--- packages/cli-ui/src/containers/ProjectContainer/projectContainer.hook.ts
+++ packages/cli-ui/src/containers/ProjectContainer/projectContainer.hook.ts
@@ -1,25 +1,37 @@
+import { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+
 import { Routes } from 'router'
 
 interface TabItem {
   key: Routes.PROJECT | Routes.PROJECT_SELECT | Routes.PROJECT_IMPORT;
-  label: 'Projects' | 'Create' | 'Import';
+  label: string;
 }
 
 interface HookProps {
   tabs:TabItem[];
+  isDarkTheme: 'actived' | '';
   activeTab: string;
   handleSetTab(tabItem: TabItem): void;
 }
 export default function useProjectContainer (): HookProps {
+  const { t } = useTranslation('project')
   const tabs: TabItem[] = [
-    { key: Routes.PROJECT, label: 'Projects' },
-    { key: Routes.PROJECT_SELECT, label: 'Create' },
-    { key: Routes.PROJECT_IMPORT, label: 'Import' }
+    { key: Routes.PROJECT, label: t('projects') },
+    { key: Routes.PROJECT_SELECT, label: t('create') },
+    { key: Routes.PROJECT_IMPORT, label: t('import') }
   ]
 
   // Router
   const history = useHistory()
+
+  // State
+  const [value, setValue] = useState(JSON.parse(localStorage.getItem('thememode')))
+
+  useEffect(() => {
+    setValue(JSON.parse(localStorage.getItem('thememode')))
+  }, [value])
 
   function handleSetTab (tabItem: any) {
     history.push(tabItem.key)
@@ -27,6 +39,7 @@ export default function useProjectContainer (): HookProps {
 
   return {
     tabs,
+    isDarkTheme: value ? 'actived' : '',
     activeTab: location.pathname,
     handleSetTab
   }
