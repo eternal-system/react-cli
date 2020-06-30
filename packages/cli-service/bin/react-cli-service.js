ad5ce323858ed7b963ddd4a2c7c1712b336c2be8
Corvax <vlvete1990@yandex.ru>
2020-06-30 19:04:36 +0300

    chore: #42 add execa

--- packages/cli-service/bin/react-cli-service.js
+++ packages/cli-service/bin/react-cli-service.js
@@ -1,1 +1,51 @@
 #!/usr/bin/env node
+const execa = require('execa')
+
+const rawArgv = process.argv.slice(2)
+console.log(rawArgv)
+
+async function cli (arg) {
+  try {
+    return await execa('react-cli-ui', [`${arg}`])
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+}
+
+// execa('react-cli-ui', [])
+async function git (status) {
+  return await execa('git', [`${status}`])
+}
+
+function cliP (argv) {
+    return new Promise((resolve, reject) => {
+        const child = execa('react-cli-ui', argv)
+        child.on('error', reject)
+        child.on('exit', code => {
+            if (code !== 0) {
+                reject(`mochapack exited with code ${code}.`)
+            } else {
+                resolve()
+            }
+        })
+    }
+)}
+
+
+// git(rawArgv[0]).then(res => {
+//   console.log(res)
+// }).catch(err => {
+//   console.error(err)
+// })
+
+// try {
+//   const res = cli(rawArgv[0])
+//   console.log(res)
+// } catch (error) {
+//   console.error(error)
+//   process.exit(1)
+// }
+
+//cli(rawArgv[0])
+// cli(rawArgv[0])
