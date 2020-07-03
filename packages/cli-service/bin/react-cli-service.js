3f1f236f5a672557f6d816910ac137d2fdb86058
Corvax <vlvete1990@yandex.ru>
2020-07-03 22:59:04 +0300

    chore: #42 add params

--- packages/cli-service/bin/react-cli-service.js
+++ packages/cli-service/bin/react-cli-service.js
@@ -2,31 +2,12 @@
 const execa = require('execa')
 
 const rawArgv = process.argv.slice(2)
-// console.log(rawArgv, __dirname)
 
-async function cli (arg) {
-  try {
-    return await execa('react-cli-ui', [`${arg}`])
-  } catch (error) {
-    console.error(error)
-    process.exit(1)
-  }
-}
-
-// execa('react-cli-ui', [])
-async function git (status) {
-  return await execa('git', [`${status}`])
-}
-
-async function create (name) {
-  const path = '/Users/vladkalachev/Desktop'
+async function create (name, path = '/Users/vladkalachev/Desktop') {
   const subprocess = execa.command(`create-react-app ${path}/${name}`)
   subprocess.stdout.pipe(process.stdout)
-
   try {
-    console.log('start process 1')
     const { stdout } = await subprocess
-    console.log('child output:', stdout)
     return stdout
   } catch (error) {
     console.error(error)
@@ -34,4 +15,4 @@ async function create (name) {
   }
 }
 
-create(rawArgv[0])
+module.exports = create(rawArgv[0], rawArgv[1])
