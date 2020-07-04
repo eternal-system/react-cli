694505428ed3c5ce1a55d3c5b3556b98abd5d81a
Corvax <vlvete1990@yandex.ru>
2020-07-04 11:45:57 +0300

    chore: #42 fix

--- packages/cli-ui/server/routes/api/projects.js
+++ packages/cli-ui/server/routes/api/projects.js
@@ -1,11 +1,29 @@
 const { Router } = require('express')
 const fs = require('fs')
+const path = require('path')
+const db = require('../../util/db')
 const router = Router()
 
+// Get list project
 router.get('/', (req, res) => {
   console.log('=> url', req.query.url)
   const folder = req.query.url || '/'
   const projects = []
+
+  //  TODO
+  // 1. проверить наличие db.json
+  // 2. если db.json нет то создаем новый db.json
+  // 3. возращаяем список projects => db.json
+
+  // 1
+  // const folderDbPath = path.normalize(__dirname + '../../../db.json')
+
+  // // проверка существования файла
+  // fs.access(folderDbPath, fs.constants.F_OK, (err) => {
+  //   console.log(`${folderDbPath} ${err ? 'не существует' : 'существует'}`)
+  // })
+  // db()
+
   fs.readdir(folder, (err, files) => {
     if (err) {
       console.error(err)
@@ -17,5 +35,10 @@ router.get('/', (req, res) => {
     res.send(projects.filter(str => !str.startsWith('.')))
   })
 })
+
+// Create new project
+// router.post('/create', () => {
+
+// })
 
 module.exports = router
