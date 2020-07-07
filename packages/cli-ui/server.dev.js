655387a449ef804a1910133c24c0a2b6cf53a603
Corvax <vlvete1990@yandex.ru>
2020-07-07 14:26:55 +0300

    chore: #57 template

--- packages/cli-ui/server.dev.js
+++ packages/cli-ui/server.dev.js
@@ -8,6 +8,8 @@ const webpackConfig = require('./webpack.config.js')
 const port = process.env.SERVER_PORT || 8080
 const app = express()
 const filePath = path.resolve(__dirname, 'dist', 'index.html')
+const timeout = require('connect-timeout')
+//const timeout = require('express-timeout-handler');
 
 /* db */
 if (!fs.existsSync('db.json')) {
@@ -15,13 +17,41 @@ if (!fs.existsSync('db.json')) {
   require('./server/util/db')
 }
 
+
+
+// app.use('/api/projects/create', (req, res, next) => {
+//     req.setTimeout(4 * 60 * 1000); // No need to offset
+
+//     req.socket.removeAllListeners('timeout'); // This is the work around
+//     req.socket.once('timeout', () => {
+//         req.timedout = true;
+//         res.status(504).send('Timeout');
+//     });
+
+//     next();
+// });
+
+// const options = {
+//   timeout: 180000
+// }
+
+// app.use(haltOnTimedout);
+// app.use(timeout.handler(options));
+
+// app.use(timeout('3m'))
 app.use(express.json({ extended: true }))
+// app.use(haltOnTimedout)
 
 app.use(require('./server/routes'))
+
+
+
 /* static server */
 if (process.env.DEV_SERVER.trim() === 'true') {
   console.log('SSR start')
-  
+  app.use(timeout(125000));
+  app.use(haltOnTimedout);
+
   app.use(webpackHotMiddleware(webpack(webpackConfig)))
 
   app.use('/', express.static(path.join(__dirname, 'dist')))
@@ -42,4 +72,17 @@ if (process.env.DEV_SERVER.trim() === 'true') {
 }
 
 app.listen(port)
+  // .use(timeout('3m'))
+  // .use((res, req, next) => {
+  //   console.log('timers!')
+  //   setTimeout(() => next(), 2000)
+  // })
+  // .use(haltOnTimedout)
+
+
+function haltOnTimedout (req, res, next) {
+  console.log('req.timedout !!!', req.timedout)
+  if (!req.timedout) next()
+}
+
 console.log(`\n Server started on port ${port} \n`)
--- packages/cli-ui/server/routes/api/projects.js
+++ packages/cli-ui/server/routes/api/projects.js
@@ -9,7 +9,46 @@ const folderDbPath = path.normalize(__dirname + '../../../../db.json')
 const adapter = new FileSync(folderDbPath)
 const db = low(adapter)
 
+const { exec, spawn } = require('child_process')
+
 const execa = require('execa')
+const shell = require('shelljs')
+
+// const timeout = require('connect-timeout')
+
+// scripts
+const craNpm = (pathProject, name) => execa.command(`npx create-react-app ${path.join('/', ...pathProject, name)} --use-npm`, {shell: true})
+const craYarn = (pathProject, name) => execa.command(`yarn create react-app ${path.join('/', ...pathProject, name)}`, {shell: true})
+
+
+
+const createReactApp = (pathProject, name) => {
+  return new Promise(resolve=>{
+    if(name){
+      shell.exec(`create-react-app ${path.join('/', ...pathProject, name)}`, () => {
+        console.log("Created react app")
+        resolve(true)
+      })
+    }else{
+      console.log("\nNo app name was provided.".red)
+      console.log("\nProvide an app name in the following format: ")
+      console.log("\ncreate-react-redux-router-app ", "app-name\n".cyan)
+        resolve(false)
+    }
+  })
+}
+
+function execShellCommand(cmd) {
+  const exec = require('child_process').exec;
+  return new Promise((resolve, reject) => {
+   exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+     console.warn(error);
+    }
+    resolve(stdout? stdout : stderr);
+   });
+  });
+ }
 
 // Get list project
 router.get('/', (req, res) => {
@@ -21,7 +60,7 @@ router.get('/', (req, res) => {
 })
 
 // Create new project
-router.post('/create', async (req, res) => {
+router.post('/create', async (req, res, next) => {
   const { 
     name, 
     path: pathProject, 
@@ -29,42 +68,87 @@ router.post('/create', async (req, res) => {
     preset = ''
   } = req.body
 
-  console.log(111111111111, req.body, path.join('/', ...pathProject, name))
-  let subprocess
-  if(manager === 'npm') {
-    subprocess = execa.command(`npx create-react-app ${path.join('/', ...pathProject, name)} --use-npm`)
-  } else {
-    subprocess = execa.command(`yarn create react-app ${path.join('/', ...pathProject, name)}`)
-  }
+  // res.setHeader('Expires', '-1')
+  // res.setHeader('Pragma', 'no-cache')
 
- // const subprocess = execa.command(`create-react-app ${path.join('/', ...pathProject, name)}`)
-  subprocess.stdout.pipe(process.stdout)
-  try {
-    const { stdout } = await subprocess
-    console.log(2222222222, stdout)
-
-    // Add item project
-    // db.get('projects').push({
-    //   id: db.get('projects').value().length + 1,
-    //   name,
-    //   path,
-    //   manager,
-    //   preset
-    // }).write()
-
-    // res.send(db.get('projects').value())
-
-    return res.status(200).json({
-      message: 'Project successfully create'
-    })
-  } catch (error) {
-    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
-    console.error(error)
-    process.exit(1)
-  }
-
+  // req.setTimeout(340000);
+ 
+  // const child = spawn(`create-react-app ${path.join('/', ...pathProject, name)}`);
+  //const child = spawn('create-react-app', [path.join('/', ...pathProject, name)]);
+  // console.log('child', child)
   
+ 
+
+  // process.on('data', (data) => {
+  //   console.log('data process =', data)
+  // })
+
+  // process.on('close', (data) => {
+  //   console.log('close process =', data)
+  // })
+
+  // console.log('324535345345345', process.stdout.pipe(child.stdout))
+  // process.stdout.pipe(process.child)
+  // process.stderr.pipe(process.child)
+
+
+
+  // child.stdout.on('data', (data) => {
+  //   console.log(`child stdout:\n${data}`);
+  // });
+
+  // child.stderr.on('data', (data) => {
+
+  //   console.log(`child stderr: ${data}`);
+  // });
+
+  // child.on('close', (code) => {
+  //   console.log(`child process exited with code ${code}`);
+
+  // });
+
+  // child.on('exit', function (code, signal) {
+  //   console.log('child process exited with ' +
+  //               `code ${code} and signal ${signal}`);
+
+  // });
+
+  try {
+     let success = await createReactApp(pathProject, name)
+    console.log('success', success)
+   
+    res.send('Project successfully create')
+    res.end()
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ 
+      message: 'Что-то пошло не так, попробуйте снова'
+    })
+  }
+ 
+
+ 
+  // if(!success) {
+  //  return false
+  // }
+
+  // if(success) {
+  //   res.send('Project successfully create')
+  //   // return res.status(200).json({
+  //   //   message: 'Project successfully create'
+  //   // })
+  // } else {
+  //   return res.status(500).json({ 
+  //     message: 'Что-то пошло не так, попробуйте снова'
+  //   })
+  // }
+ 
 })
+
+function haltOnTimedout (req, res, next) {
+  console.log(2342343242343, req.timedout)
+  if (!req.timedout) next()
+}
 
 // Get project by Id
 router.get('/:id', (req, res) => {
