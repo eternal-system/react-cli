17bcaf5541c18efc6db76d36485136f9b26b206c
Corvax <vlvete1990@yandex.ru>
2020-07-11 17:37:56 +0300

    chore: #63 add app connector ws

--- packages/cli-ui/src/App.tsx
+++ packages/cli-ui/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { I18nextProvider } from 'react-i18next'
 import ReactNotification from 'react-notifications-component'
@@ -14,8 +14,21 @@ import 'react-notifications-component/dist/theme.css'
 import css from './style/main.module.scss'
 
 export default function App () {
+
+  const URL = 'ws://localhost:8082'
   const settings = useSettings()
   const styles = cn(settings.darkTheme ? 'dark' : 'ligth', css.appContainer)
+  const ws = useRef(null)
+
+  useEffect(() => {
+    ws.current = new WebSocket(URL);
+    ws.current.onopen = () => console.log("ws opened");
+    ws.current.onclose = () => console.log("ws closed");
+
+    return () => {
+        ws.current.close();
+    };
+}, []);
 
   return (
     <I18nextProvider i18n={i18n}>
