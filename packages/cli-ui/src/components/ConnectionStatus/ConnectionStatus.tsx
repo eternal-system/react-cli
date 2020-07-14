9c362f22d8828d0713034cb4fe19398f8ba2b810
Corvax <vlvete1990@yandex.ru>
2020-07-14 22:43:48 +0300

    chore: #67 add status step

--- packages/cli-ui/src/components/ConnectionStatus/ConnectionStatus.tsx
+++ packages/cli-ui/src/components/ConnectionStatus/ConnectionStatus.tsx
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { SettingsContext } from '../../context'
 
 export default function ConnectionStatus () {
   const { socket } = React.useContext(SettingsContext)
 
+  const [connected, setConnected] = useState(false)
+  const [status, setStatus] = useState('show')
+
   useEffect(() => {
     socket.on('connect', () => {
-        console.log('connect')
+        setStatus('show')
+        setConnected(true)
+        setTimeout(() => {
+            setStatus('hidden')
+        }, 300)
+        socket.on('disconnect', function() {
+            setStatus('show')
+            setTimeout(() => {
+                setStatus('hidden')
+            }, 300)
+            setConnected(false)
+        })
     })
     return () => {
         socket.off('connect')
+        socket.off('disconnect')
+        setConnected(false)
       }
-  }, [])
+  }, [status])
 
   return (
-    <div className="content">
+    <div className={`content ${status}`}>
       {connected ? (<div className="connected">
         Connected
       </div>) : (<div className="disconnected">
