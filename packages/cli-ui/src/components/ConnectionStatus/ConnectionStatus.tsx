8eac91eafd30e06d9aca4bcdcdfe3a82440d9d4a
Corvax <vlvete1990@yandex.ru>
2020-07-15 10:14:29 +0300

    chore: #67 add scss module

--- packages/cli-ui/src/components/ConnectionStatus/ConnectionStatus.tsx
+++ packages/cli-ui/src/components/ConnectionStatus/ConnectionStatus.tsx
@@ -1,42 +1,38 @@
 import React, { useState, useEffect } from 'react'
 import { SettingsContext } from '../../context'
+import css from './style.module.scss'
 
 export default function ConnectionStatus () {
   const { socket } = React.useContext(SettingsContext)
 
-  const [connected, setConnected] = useState(false)
+  const [connected, setConnected] = useState(true)
   const [status, setStatus] = useState('show')
 
   useEffect(() => {
     socket.on('connect', () => {
         setStatus('show')
-        console.log('connect')
         setConnected(true)
         setTimeout(() => {
             setStatus('hidden')
-        }, 500)
+        }, 700)
         socket.on('disconnect', function() {
-            setStatus('show')
             console.log('disconnect')
-            setTimeout(() => {
-                setStatus('hidden')
-            }, 500)
+            setStatus('show')
             setConnected(false)
         })
     })
     return () => {
         socket.off('connect')
         socket.off('disconnect')
-        console.log('disconnect')
         setConnected(false)
       }
   }, [])
 
   return (
-    <div className={`content ${status}`}>
-      {connected ? (<div className="connected">
+    <div className={`${css.content} ${status === 'show' ? css.show : css.hidden}`}>
+      {connected ? (<div className={css.connected}>
         Connected
-      </div>) : (<div className="disconnected">
+      </div>) : (<div className={css.disconnected}>
         Disconnected
       </div>)}
     </div>
