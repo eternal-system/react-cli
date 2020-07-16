dc707fa6cb5dd2f4d79021f7f576d5f15577eb00
Corvax <vlvete1990@yandex.ru>
2020-07-16 16:40:11 +0300

    chore: #64 #62 add filter style

--- packages/cli-ui/src/components/ProjectManager/ProjectFilter.tsx
+++ packages/cli-ui/src/components/ProjectManager/ProjectFilter.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import { Project } from '../../pages/Projects'
+import css from './style.module.scss'
+import { Input } from 'common'
 
 interface ProjectFilter {
       projects: Project[]
 }
 
 export default function ProjectFilter ({ projects }) {
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value)
+  }
+
   return (
-    <div className="project__filter">
-          Filter
+    <div className={css.filter}>
+        <div className={css.filter.filter}>
+          <input 
+            className={css.input}
+            onChenge={handleChange}
+            type="text"
+          />
+         </div>
     </div>
   )
 }
