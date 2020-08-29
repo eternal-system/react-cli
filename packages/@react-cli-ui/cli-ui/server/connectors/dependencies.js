6724a47005acbd53277755a55d5b016afe473ef2
Corvax <vlvete1990@yandex.ru>
2020-08-29 17:52:18 +0300

    chore: #130 add getVersion

--- packages/@react-cli-ui/cli-ui/server/connectors/dependencies.js
+++ packages/@react-cli-ui/cli-ui/server/connectors/dependencies.js
@@ -1,5 +1,8 @@
 const path = require('path')
 const fs = require('fs')
+const fetch = require('node-fetch')
+const semver = require('semver')
+
 const { resolveModuleRoot } = require('../util/resolve-path')
 const { resolveModule } = require('../util/modules')
 
@@ -42,10 +45,35 @@ class DependenciesApi extends StaticMethods {
         versionRange: deps[id],
         installed: this.isInstalled({ id, file }),
         website: this.getLink({ id, file }),
+       // wanted: this.getVersion(id).wanted,
+       // latest: this.getVersion(id).latest,
         type,
         baseFir: file
       })
     )
+  }
+
+  getVersion (id) {
+    console.log(id)
+    let latest, wanted
+    const metadata = this.getMetadata(id)
+    if(metadata) {
+      latest = metadata['dist-tags'] ? metadata['dist-tags'].latest : ''
+      const versions = Array.isArray(metadata.versions) ? metadata.versions : Object.keys(metadata.versions)
+      wanted = semver.maxSatisfying(versions, versionRange)
+    }
+    return {
+      latest,
+      wanted
+    }
+  }
+
+  getMetadata(pkg) {
+    return fetch(`http://registry.npmjs.org/${pkg}`)
+            .then(res => res.json())
+            .then(json => {
+              return json
+            })
   }
 
   isInstalled ({ id, file }) {
@@ -74,6 +102,58 @@ class DependenciesApi extends StaticMethods {
     }
     return {}
   }
+
+  // getVersion ({ id, installed, versionRange, baseDir }) {
+  //   let current
+
+  //   // Read module package.json
+  //   if (installed) {
+  //     const pkg = this.readPackage({ id, file: baseDir })
+  //     current = pkg.version
+  //   } else {
+  //     current = null
+  //   }
+  
+  //   // Metadata
+  //   let latest, wanted
+  //   const metadata = await getMetadata(id)
+  //   if (metadata) {
+  //     latest = metadata['dist-tags'].latest
+  
+  //     const versions = Array.isArray(metadata.versions) ? metadata.versions : Object.keys(metadata.versions)
+  //     wanted = semver.maxSatisfying(versions, versionRange)
+  //   }
+  
+  //   if (!latest) latest = current
+  //   if (!wanted) wanted = current
+  
+  //   return {
+  //     current,
+  //     latest,
+  //     wanted,
+  //     range: versionRange,
+  //   }
+  // }
+
+  // async function getMetadata (id, context) {
+  //   let metadata = metadataCache.get(id)
+  //   if (metadata) {
+  //     return metadata
+  //   }
+  
+  //   try {
+  //     metadata = await (new PackageManager({ context: cwd.get() })).getMetadata(id)
+  //   } catch (e) {
+  //     // No connection?
+  //   }
+  
+  //   if (metadata) {
+  //     metadataCache.set(id, metadata)
+  //     return metadata
+  //   } else {
+  //     log('Dependencies', chalk.yellow('Can\'t load metadata'), id)
+  //   }
+  // }
 
   install () {
 
