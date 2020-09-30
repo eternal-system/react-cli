293ea85b2e17cba4a01af80aa47038c02f4727f8
Corvax <vlvete1990@yandex.ru>
2020-09-30 10:50:29 +0300

    chore: #174 delete routes

--- packages/@react-cli-ui/cli-ui/server/routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-
-// router.use('/folders', require('./folders'))
-// router.use('/projects', require('./projects'))
-router.use('/kill', require('./kill'))
-
-module.exports = router
